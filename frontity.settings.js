ee6ee8a135f16a2e2c12bd4139cf70a1b0366532
Cristhian Hernández <68720424+cristhianjhlweb@users.noreply.github.com>
2021-07-26 10:29:40 -0500

    Update frontity.settings.js

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   "name": "cristhianjhl",
   "state": {
     "frontity": {
-      "url": "https://cristhianjhl.com/",
+      "url": "https://wp.cristhianjhl.com/",
       "title": "Cristhian Hernández Desarrollador Frontend Freelancer en Lima",
       "description": "Tienes un negocio digital en mente, pero aun no sabes como empezar. Te ayudo a que tu producto o servicio tenga prencencia en Internet."
     }
