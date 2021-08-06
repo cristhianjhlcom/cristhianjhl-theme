6fcef9372075355bf5944c9994331c574fecf04b
Cristhian <cristhianjhl.web@gmail.com>
2021-08-06 13:28:44 -0500

    fix/remove body background image

--- packages/cristhianjhl-theme/src/pages/Root/Root.styles.js
+++ packages/cristhianjhl-theme/src/pages/Root/Root.styles.js
@@ -2,8 +2,8 @@ import { styled } from "frontity";
 
 export const Main = styled.main`
     min-height: 100%;
-    background-image: url(https://www.frontendmentor.io/static/images/bg-newsletter.svg);
-    background-position: 150% bottom;
+    /* background-image: url(https://www.frontendmentor.io/static/images/bg-newsletter.svg);
+    background-position: 150% bottom; */
 `;
 
 export const Container = styled.div`
